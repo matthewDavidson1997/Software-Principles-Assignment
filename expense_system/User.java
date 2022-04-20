5d5321783945ba9c864b195cff0fc297244b7d43
James Davidson <j.davidson@vernalis.com>
2022-04-20 15:57:21 +0100

    Added comments

--- expense_system/User.java
+++ expense_system/User.java
@@ -2,14 +2,17 @@ package expense_system;
 
 public class User {
 
+    // fields
     private String firstName;
     private String lastName;
 
+    // constructor
     public User(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
+    // methods
     public String toString() {
         return this.firstName + " " + this.lastName;
     }
