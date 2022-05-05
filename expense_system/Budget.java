f3a1be064ecaacdd0297d62818e7845f6c6b6afb
James Davidson <j.davidson@vernalis.com>
2022-05-05 22:11:53 +0100

    Fairly significant changes

--- expense_system/Budget.java
+++ expense_system/Budget.java
@@ -9,43 +9,60 @@ import java.util.List;
  */
 public class Budget {
 
-    // fields
+    // FIELDS
     private double startingAmount;
     private double currentAmount;
-    private List<Expense> expenses;
+    private List<Expense> expenses = new ArrayList<>();
 
-    // constructor
+    // CONSTRUCTOR
     public Budget(double startingAmount) {
         this.startingAmount = startingAmount;
         this.currentAmount = startingAmount;
-        this.expenses = new ArrayList<>();
     }
 
-    // methods
+    // METHODS
+
+    // to string
+    public String toString() {
+        return this.startingAmount + " (current = " + this.currentAmount + ")";
+    }
+
+    // getters
+    public double getCurrentAmount() {
+        return this.currentAmount;
+    }
+    public List<Expense> getExpenses() {
+        return this.expenses;
+    }
+
+    // other methods
+
+    // A method to return expenses relating to a particular user
+    public List<Expense> getUserExpenses(User user) {
+        List<Expense> userExpenses = new ArrayList<>();
+        for (Expense expense : this.expenses) {
+            if (expense.getUser() == user) {
+                userExpenses.add(expense);
+            }
+        }
+        return userExpenses;
+    }
+
+    // A method to add funds to the budget (we don't keep a history of these events)
     public void addFunds(double amount) {
         this.currentAmount += amount;
     }
 
+    // A method to record an expense against the budget
     public void recordExpense(Expense expense) {
         this.currentAmount -= expense.getAmount();
         this.expenses.add(expense);
     }
 
-    public double getCurrentAmount() {
-        return this.currentAmount;
-    }
-
-    public List<Expense> getExpenses() {
-        return this.expenses;
-    }
-
+    // A simple method to print all expenses
     public void printExpenses() {
-        for(int i = 0; i < this.expenses.size(); i++) {
-            System.out.println(this.expenses.get(i));
+        for (Expense expense : this.expenses) {
+            System.out.println(expense);
         }
-    }
-
-    public String toString() {
-        return this.startingAmount + " (current = " + this.currentAmount + ")";
     }
 }
